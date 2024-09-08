78482ec1cf83af1411ab50c7f1542fca3c17d856
Fernando Omar Salazar <112194084+Feromar98@users.noreply.github.com>
2024-09-08 15:28:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,34 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# Streamlit page config
 st.set_page_config(page_title="Gen AI: Analysis Tool", page_icon="🤖")
 st.title("🤖 Gen AI: Analysis Tool")
 
+# Clear the Submit Button State:
 def clear_submit():
     """
-    Clear the Submit Button State
     Returns:
 
     """
     st.session_state["submit"] = False
-
+    
+# OpenAI config
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
+if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+        
+llm = ChatOpenAI(
+        model = "gpt-4o-mini", 
+        temperature = 0, 
+        openai_api_key = openai_api_key,
+        streaming = True,
+        max_tokens = 3000
+        )
+
+#Uploaded file config
 uploaded_file = st.file_uploader(
     "Upload a CSV file",
     type="CSV",
@@ -35,28 +49,19 @@ if uploaded_file:
 else:
     st.write("Please upload a CSV file")
 
+# Chat session state
 if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:  
     st.chat_message(msg["role"]).write(msg["content"])
-    
+
+# Saves prompt on session state    
 if prompt := st.chat_input(placeholder="What is this data about?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
         
-    llm = ChatOpenAI(
-        model = "gpt-4o-mini", 
-        temperature = 0, 
-        openai_api_key = openai_api_key,
-        streaming = True,
-        max_tokens = 3000
-        )
-        
+    # Creates agent able to analize pandas dataframes
     pandas_df_agent = create_pandas_dataframe_agent(
         llm,
         df,
@@ -67,8 +72,11 @@ if prompt := st.chat_input(placeholder="What is this data about?"):
     )
         
     with st.chat_message("assistant"):
+        # Defining sync Streamlit Callback Handler
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
         response = pandas_df_agent.invoke(st.session_state.messages, callbacks=[st_cb])
         st.session_state.messages.append({"role": "assistant", "content": response["output"]})
+        #Show response
         st.write(response["output"])
+
 
