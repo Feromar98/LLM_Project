f6d370ff3746a42005a103d33133870dee88c1f2
Fernando Omar Salazar <112194084+Feromar98@users.noreply.github.com>
2024-09-08 03:32:52 -0600

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,80 @@
+#test OpenAI API Key = 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
+# 0. Imports
+
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI
+from langchain.callbacks.streamlit import StreamlitCallbackHandler
+
+import streamlit as st
+import pandas as pd
+
+
+
+st.set_page_config(page_title="LangChain: Chat with Pandas DataFrame", page_icon="🦜")
+st.title("🦜 LangChain: Chat with Pandas DataFrame")
+
+def clear_submit():
+    """
+    Clear the Submit Button State
+    Returns:
+
+    """
+    st.session_state["submit"] = False
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+uploaded_file = st.file_uploader(
+    "Upload a CSV file",
+    type="CSV",
+    help = "Only supports CSV files",
+    on_change=clear_submit
+)
+
+df = pd.DataFrame([])
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+else:
+    st.write("Please upload a CSV file")
+
+if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    
+st.write(st.session_state.messages)
+
+for msg in st.session_state.messages:  
+    st.write(msg)
+    st.chat_message(msg["role"]).write(msg["content"])
+    
+if prompt := st.chat_input(placeholder="What is this data about?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+        
+    llm = ChatOpenAI(
+        model = "gpt-4o-mini", 
+        temperature = 0, 
+        openai_api_key = openai_api_key,
+        streaming = True,
+        max_tokens = 3000
+        )
+        
+    pandas_df_agent = create_pandas_dataframe_agent(
+        llm,
+        df,
+        verbose=True,
+        allow_dangerous_code=True,
+        agent_type="openai-functions",
+        handle_parsing_errors=True,
+    )
+        
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = pandas_df_agent.invoke(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response["output"]})
+        st.write(response["output"])
+
